c718286888b8743661bf00c66d8a0c59a8abeb57
a5768549 <44581221+a5768549@users.noreply.github.com>
2020-06-19 20:08:34 +0800

    Initializing

---
+++ Tutorial_1_Helloworld/Tutorial_1_Helloworld.ino
@@ -0,0 +1,19 @@
+/*
+ * DSTIKE ESP32 Watch(WROOM-32)
+ * Copyright 2020 a5768549
+ *
+ * Tutorial 1 Hello World!
+ * 
+ * GitHub Project：https://github.com/a5768549/DSTIKE-ESP32-Watch-Tutorial
+ */
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("Hello world!");
+  delay(1000);
+}
---
+++ Tutorial_2_LED/Tutorial_2_LED.ino
@@ -0,0 +1,24 @@
+/*
+ * DSTIKE ESP32 Watch(WROOM-32)
+ * Copyright 2020 a5768549
+ *
+ * Tutorial 2 LED
+ * 
+ * GitHub Project：https://github.com/a5768549/DSTIKE-ESP32-Watch-Tutorial
+ */
+
+//Pin 27 is back side LED(High light LED)
+#define LED 27
+ 
+void setup() 
+{
+  pinMode(LED,OUTPUT);
+}
+ 
+void loop() 
+{
+  digitalWrite(LED,HIGH);
+  delay(500);
+  digitalWrite(LED,LOW);
+  delay(500);
+}
