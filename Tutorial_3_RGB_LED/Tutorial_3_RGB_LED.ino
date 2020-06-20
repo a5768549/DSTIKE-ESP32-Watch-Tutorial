36c6a2bb4d011278e50d87385b2e24b71bd31e51
a5768549 <44581221+a5768549@users.noreply.github.com>
2020-06-20 21:56:33 +0800

    Initializing

---
+++ Tutorial_3_RGB_LED/Tutorial_3_RGB_LED.ino
@@ -0,0 +1,70 @@
+/*
+ * DSTIKE ESP32 Watch(WROOM-32)
+ * Copyright 2020 a5768549
+ *
+ * Tutorial 3 RGB_LED
+ * CPU Frequency must set 160MHZ
+ * GitHub Project：https://github.com/a5768549/DSTIKE-ESP32-Watch-Tutorial
+ */
+#include <Adafruit_NeoPixel.h>
+
+//Pin 33 is front side LED(RGB_LED)
+#define RGB_LED 33
+Adafruit_NeoPixel pixels(1, RGB_LED, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+  pixels.begin();
+  Serial.begin(9600);
+}
+ 
+void loop() 
+{
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0)); //Red
+  pixels.show();
+  delay(500);
+
+  pixels.setPixelColor(0, pixels.Color(0, 255, 0)); //Green
+  pixels.show();
+  delay(500);
+
+  pixels.setPixelColor(0, pixels.Color(0, 0, 255)); //Blue
+  pixels.show();
+  delay(500);
+
+  pixels.setPixelColor(0, pixels.Color(255, 255, 0)); //Yellow
+  pixels.show();
+  delay(500);
+
+  pixels.setPixelColor(0, pixels.Color(255, 0, 255)); //Purple
+  pixels.show();
+  delay(500);
+
+  pixels.setPixelColor(0, pixels.Color(0, 255, 255)); //Cyan
+  pixels.show();
+  delay(500);
+
+  pixels.setPixelColor(0, pixels.Color(255, 255, 255)); //White
+  pixels.show();
+  delay(500);
+
+  pixels.setPixelColor(0, pixels.Color(0, 0, 0)); //no
+  pixels.show();
+  delay(500);
+
+  for(int i = 0;i<256;i++)
+  {
+    pixels.setPixelColor(0, pixels.Color(i, i, i)); //White Breathing light 0~255
+    pixels.show();
+    delay(30);
+    Serial.println(i);
+  }
+
+  for(int i = 255;i>=0;i--)
+  {
+    pixels.setPixelColor(0, pixels.Color(i, i, i)); //White Breathing light 255~0
+    pixels.show();
+    delay(30);
+    Serial.println(i);
+  }
+}
---
+++ Tutorial_4_Buzzer_and_button/Tutorial_4_Buzzer_and_button.ino
@@ -0,0 +1,78 @@
+#include <Tone32.h>
+
+/*
+ * DSTIKE ESP32 Watch(WROOM-32)
+ * Copyright 2020 a5768549
+ *
+ * Tutorial 4 Buzzer_and_button
+ * 
+ * ESP Tone Lib：https://github.com/lbernstone/Tone
+ * 
+ * GitHub Project：https://github.com/a5768549/DSTIKE-ESP32-Watch-Tutorial
+ */
+
+#define Buzzer 32
+//Navigation Button
+#define Nav_Up     19
+#define Nav_Center 18
+#define Nav_Down    5
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(Nav_Up    , INPUT_PULLUP);
+  pinMode(Nav_Center, INPUT_PULLUP);
+  pinMode(Nav_Down  , INPUT_PULLUP); //Press = 0
+  pinMode(Buzzer    , OUTPUT);
+}
+ 
+void loop() 
+{
+  if(digitalRead(Nav_Up) == LOW)
+  {
+    tone(Buzzer, NOTE_C4, 250, 0); //Do
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_D4, 250, 0); //Re
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_E4, 250, 0); //Mi
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_F4, 250, 0); //Fa
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_G4, 250, 0); //So
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_A4, 250, 0); //La
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_B4, 250, 0); //Si
+    noTone(Buzzer, 0);
+  }
+
+  if(digitalRead(Nav_Down) == LOW)
+  {
+    tone(Buzzer, NOTE_B4, 250, 0); //Si
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_A4, 250, 0); //La
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_G4, 250, 0); //So
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_F4, 250, 0); //Fa
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_E4, 250, 0); //Mi
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_D4, 250, 0); //Re
+    noTone(Buzzer, 0);
+    tone(Buzzer, NOTE_C4, 250, 0); //Do
+    noTone(Buzzer, 0);
+  }
+
+  if(digitalRead(Nav_Center) == LOW)
+  {
+    tone(Buzzer, NOTE_B4, 250, 0); //Si
+    noTone(Buzzer, 0);
+    delay(500);
+    tone(Buzzer, NOTE_B4, 250, 0); //Si
+    noTone(Buzzer, 0);
+    delay(500);
+    tone(Buzzer, NOTE_B4, 250, 0); //Si
+    noTone(Buzzer, 0);
+  }
+}
